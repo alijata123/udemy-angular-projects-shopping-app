882f1baeee315809b64c932b4e5365c20aa06326
Ali Jata <alijataaa@gmail.com>
2019-03-27 12:04:50 +0200

    first commit of shopping App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './header/shopping-list/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/shopping-list/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/shopping-list/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
